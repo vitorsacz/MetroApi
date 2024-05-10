e3cbb17954f666210c8a9b35dd297f3e657d585e
Vitor Santos <vitorsacz@gmail.com>
2024-05-10 08:50:45 -0300

    Recebendo a operacao das linhas

--- server/server.py
+++ server/server.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
 
 #api/home = tela inicial, neste caso retorna um JSON
 @app.route("/api/home", methods=['GET'])
 def return_home():
-    return jsonify({
-        'message' : 'Hello World',
-        'people': ['Vitor', 'Lucca', 'Arnaldo']
+    return jsonify({ 
+            'message' : 'Hello World',
+            'people': ['Vitor', 'Lucca', 'Arnaldo']     
     })
+
 
 
 if __name__ == '__main__':
